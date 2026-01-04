5e07d44ff2895d3870fb89f08cb516960619cb06
Andrey <inokatre08@gmail.com>
2026-01-04 19:47:34 +0200

    Solution3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,24 @@
 'use strict';
 
-const body = document.body;
-
 const firstPromise = new Promise((resolve, reject) => {
   const timerId = setTimeout(() => {
     // eslint-disable-next-line prefer-promise-reject-errors
     reject('First promise was rejected');
   }, 3000);
 
-  body.addEventListener('click', () => {
+  document.addEventListener('click', () => {
     clearTimeout(timerId);
     resolve('First promise was resolved');
   });
 });
 
 const secondPromise = new Promise((resolve) => {
-  body.addEventListener('contextmenu', (e) => {
+  document.addEventListener('contextmenu', (e) => {
     e.preventDefault();
     resolve('Second promise was resolved');
   });
 
-  body.addEventListener('click', (e) => {
+  document.addEventListener('click', (e) => {
     resolve('Second promise was resolved');
   });
 });
@@ -35,12 +33,12 @@ const thirdPromise = new Promise((resolve) => {
     }
   };
 
-  body.addEventListener('click', (e) => {
+  document.addEventListener('click', (e) => {
     left = true;
     tryResolv();
   });
 
-  body.addEventListener('contextmenu', (e) => {
+  document.addEventListener('contextmenu', (e) => {
     e.preventDefault();
     right = true;
     tryResolv();
